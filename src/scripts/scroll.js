73c597af615c52f114432ee6016a5179f52a0fd8
Kaitlyn <kacysuzmeyan@protonmail.ch>
2022-10-24 11:49:04 +0100

    add spoken languages

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -7,6 +7,6 @@ document.addEventListener('scroll', function (e) {
     ele.style.height = height + "vh";
     var opacity = (300 - window.scrollY) / (500/0.6) + 1;
     ele.style.border = `1px solid rgba(0, 0, 0, ${opacity})`
-    var distance = (window.scrollY - 300) / 75 + 5;
+    var distance = (window.scrollY - 300) / 75 + 6;
     ele.style.left = `${distance}vw`
 });
