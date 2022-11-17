6a2a9fe75caffaf9c2037817657eac97fff1b4e9
KaitlynEthylia <91832471+KaitlynEthylia@users.noreply.github.com>
2022-11-17 16:08:14 +0000

    edit reviews

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -3,10 +3,9 @@ const element = document.getElementById("score");
 AOS.init();
 
 document.addEventListener('aos:in:reviews', ({ detail }) => {
-    for (var index = 0; index < 5; index+=0.1) {
+    for (var index = 0; index < 3.3; index+=0.1) {
         task(index);
     }
-    console.log(element);
 });
 
 function task(i) {
